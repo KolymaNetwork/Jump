b2e23c4240beb638148e53589e9babf2dab90ce2
Developer1 <nedelyanet@proton.me>
2026-09-25 15:04:52 -0400

    Current version of our Jump script.

--- jump.php
+++ jump.php
@@ -1,50 +1,107 @@
 <?php
-define('REMOTE_ADDR', $_SERVER['REMOTE_ADDR']); // change for cloudflare
-define('_RETURN', 'index.html');
-
-// URL match
- $URL = strip_tags(urldecode($_SERVER['QUERY_STRING']));
+$URL = strip_tags(urldecode($_SERVER['QUERY_STRING']));
 if (!preg_match('/^[a-zA-Z]+:\/\//i', $URL))  {
 	die("Invalid URL");
 }
 
+$trustedarray = [
+];
+
+$untrustedarray = [
+];
+
+$forceredirects = [
+];
+
+foreach ($forceredirects as $keyword => $target) {
+	if (stripos($URL, $keyword) !== false) {
+		header("Location: " . $target);
+		exit;
+	}
+}
+
+foreach ($trustedarray as $domain) {
+	if (preg_match('/^[a-zA-Z]+:\/\/([a-zA-Z0-9-]+\.)*' . preg_quote($domain, '/') . '/i', $URL)) {
+		header("Location: " . $URL);
+		exit;
+	}
+}
+
+$untrusted = false;
+foreach ($untrustedarray as $domain) {
+	if (preg_match('/^[a-zA-Z]+:\/\/([a-zA-Z0-9-]+\.)*' . preg_quote($domain, '/') . '/i', $URL)) {
+		$untrusted = true;
+		break;
+	}
+}
+
+$REDIRECT_DELAY = 5;
+$URL_HTML = htmlspecialchars($URL, ENT_QUOTES, 'UTF-8');
+$URL_JS   = json_encode($URL);
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>Jump</title>
+	<?php if (!$untrusted) { ?>
+	<meta http-equiv="refresh" content="<?= $REDIRECT_DELAY ?>;url=<?= $URL_HTML ?>">
+	<?php } ?>
+	<title>★Jump★ | Kolyma Network</title>
 	<meta name="robots" content="nofollow,noarchive" />
+	<?php if (!$untrusted) { ?>
 	<script>
+		var redirectURL = <?= $URL_JS ?>;
+                
+                if (window.location.hash) {
+		        redirectURL += window.location.hash;
+	        }
+                
+		var seconds = <?= $REDIRECT_DELAY ?>;
+		function countdown() {
+			var el = document.getElementById("countdown");
+			if (el) {
+				el.textContent = seconds;
+			}
+			if (seconds <= 0) {
+				window.location.href = redirectURL;
+				return;
+			}
+			seconds--;
+			setTimeout(countdown, 1000);
+		}
 		function l() {
 			var link = document.getElementById("linkto");
+		if (link) {
+			link.href = redirectURL;
+			link.textContent = redirectURL;
 			link.focus();
 		}
-
+			countdown();
+		}
 	</script>
-	<script src="https://kncdn.org/knirp.php?js"></script>
+	<?php } ?>
+        <style>
+        body {
+        font-family: MS PGothic, Mona;
+        }
+        </style>
 </head>
-
-<body onload="l();" link="#0000ee" vlink="#0000EE" text="#000" bgcolor="#ffffff" alink="#FF0000">
-	<b><a href="<?=$URL?>"><?=$URL?></a></b><br>
-	 Jumping to new URL... click the link to continue
-	 <p align="RIGHT"><b><a href="https://www.kolyma.org/join">[★]Developers, Moderators, Janitors, etc wanted! Enlist today! Join the 50 cent army!</a></b></p>
-	 <p align="RIGHT"><b><a href="https://nss.kolyma.org/join">[★] Join the NSS</a></b></p>
-	     <hr>
-		<iframe src='//kncdn.org/fonts.php' frameborder='0' scrolling='no' width='728' height='90'></iframe><br />
-		<iframe src='//kncdn.org/fonts.php' frameborder='0' scrolling='no' width='728' height='90'></iframe><br />
-		<iframe src='//kncdn.org/fonts.php' frameborder='0' scrolling='no' width='728' height='90'></iframe><br />
-	
-	
+<body <?php if (!$untrusted) { ?>onload="l();"<?php } ?> link="#0000ee" vlink="#0000EE" text="#000" bgcolor="#EEEEEE" alink="#FF0000">
+        <h1>★Jump@<font color="red">K</font>olymaNET★</h1>
+	<b><a id="linkto" href="<?php echo $URL_HTML; ?>"><?php echo $URL_HTML; ?></a></b><br>
+	<?php if ($untrusted) { ?>
+	<i>Jumping to URL... Click the link to continue.</i>
+	<?php } else { ?>
+	<i>Jumping to URL in <span id="countdown"><?php echo $REDIRECT_DELAY; ?></span>s... Click the link to continue.</i>
+	<?php } ?>
+	<?php if ($untrusted) { ?>
+	<br><font color="red"><b>ⓘ This website may be unsafe.</b></font>
+	<?php } ?>
+	 <p align="RIGHT"><b><a href="//t.me/KolymaOfficial">[★] Kolyma Network Telegram</a></b></p>
+	 <p align="RIGHT"><b><a href="//news.kolyma.moe">[★] Kolyma News Navigator</a></b></p>
+         <hr>
+         <script async src="https://telegram.org/js/telegram-widget.js?24" data-telegram-post="KolymaOfficial/10" data-width="100%"></script>
+         <hr>
+         <p align="RIGHT"><b><font color="#080">★Jump★ | Kolyma Network</font></b></p>
 </body>
-
 </html>
-<?php
-exit;
-
-function error($err) { global $msg, $URL;
-	$msg = $err;
-	$URL = '';
-}
-?>
